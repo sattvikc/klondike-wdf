994376d5c0e78a0a543a548c30e4a4699d47e393
sattvik <sattvik@455121cc-eedf-11dd-924c-edcbac861201>
2009-02-08 12:48:05 +0000

    Added base loader (index.php) Added main settings (main.yaml) Added 4 libraries (cache, file, mysqldb, theme) Added a default theme (white) Added sys directory with yaml parser Added cache directory under var

---
+++ lib/mysqldb.php
@@ -0,0 +1,17 @@
+<?php if(!defined('KLONDIKE_VER')) die("Access denied! Are you trying to hack???"); ?>
+<?php
+    function mysqldb_fetch_all($query) {
+        global $_SETTINGS;
+        $result = array();
+        $db = mysql_connect($_SETTINGS['database']['host'], $_SETTINGS['database']['user'], $_SETTINGS['database']['pass']) or die ('Cannot connect to MySQL!');
+        mysql_select_db( $_SETTINGS['database']['name'] ) or die ("Cannot select database!");
+        $res = mysql_query($query) or die ("Can't query database!");
+        while ($row = mysql_fetch_array($res)) {
+            array_push($result, $row);
+        }
+        mysql_free_result($res);
+        mysql_close();
+        return $result;
+    }
+
+?>
---
+++ share/themes/white/body.php
@@ -0,0 +1,22 @@
+        <div id="container">
+            <div id="header">
+                <div id="logo">
+                    <span style="font-size: 2.2em; color: #110;">Klondike</span>-<span style="font-size: 1.6em; color:#000;">WDF</span><br />
+                    <span style="font-size: 1em; font-style: italic; color: #222;"><b>WebDev Simplified...</b></span>
+                </div>
+                <div id="nav">
+<?php //basic_loadMenu('mainmenu'); ?>
+                </div>
+            </div>
+            <table cellspacing="0" cellpadding="0" id="main"><tr>
+                <td id="col1" class="col">
+<?php //region_load("left"); ?>
+                </td>
+                <td id="col2" class="col">
+<?php //region_load("content"); ?>
+                </td>
+                <td id="col3" class="col">
+<?php //region_load("right"); ?>
+                </td>
+            </tr></table>
+        </div>
