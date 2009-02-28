3b4fd9b8ebbb59a08d0f4f5c550260395a66c29c
sattvik <sattvik@455121cc-eedf-11dd-924c-edcbac861201>
2009-02-28 13:41:57 +0000

    Need to work on App Loader. Region loader is fairly complete.

--- share/themes/white/body.php
+++ share/themes/white/body.php
@@ -1,22 +1,22 @@
         <div id="container">
             <div id="header">
                 <div id="logo">
-                    <span style="font-size: 2.2em; color: #110;">Klondike</span>-<span style="font-size: 1.6em; color:#000;">WDF</span><br />
+                    <span style="font-size: 2.2em; color: #110;"><?php echo $_SETTINGS['basic']['title'][0]; ?></span>-<span style="font-size: 1.6em; color:#000;"><?php echo $_SETTINGS['basic']['title'][1]; ?></span><br />
                     <span style="font-size: 1em; font-style: italic; color: #222;"><b>WebDev Simplified...</b></span>
                 </div>
                 <div id="nav">
-<?php //basic_loadMenu('mainmenu'); ?>
+<?php region_theme_load('menu'); ?>
                 </div>
             </div>
             <table cellspacing="0" cellpadding="0" id="main"><tr>
                 <td id="col1" class="col">
-<?php //region_load("left"); ?>
+<?php region_theme_load("left"); ?>
                 </td>
                 <td id="col2" class="col">
-<?php //region_load("content"); ?>
+<?php region_theme_load("content"); ?>
                 </td>
                 <td id="col3" class="col">
-<?php //region_load("right"); ?>
+<?php region_theme_load("right"); ?>
                 </td>
             </tr></table>
         </div>
