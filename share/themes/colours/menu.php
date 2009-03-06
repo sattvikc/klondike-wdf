82717970f1aa1734e61e1650442816a9e706ca8f
sattvik <sattvik@455121cc-eedf-11dd-924c-edcbac861201>
2009-03-06 11:09:49 +0000

    Added new theme. (needs improvement)

---
+++ share/themes/colours/menu.php
@@ -0,0 +1,2 @@
+<?php if(isset($menucount)) $menucount += 1; else $menucount = 1; ?>
+<li class="menu<?php echo $menucount; ?>"><a href="<?php echo $content['url']; ?>"><?php echo $content['text']; ?></a></li>
--- share/themes/white/menu.php
+++ share/themes/white/menu.php
@@ -1,1 +1,1 @@
-<li><a href="<?php echo $content['url']; ?>"><?php echo $content['text']; ?></a></li>
+<li <?php if(isset($content['class'])) echo 'class="' . $content['class'] . '"'; ?>><a href="<?php echo $content['url']; ?>"><?php echo $content['text']; ?></a></li>
