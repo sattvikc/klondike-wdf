e65fd94975cf9ef4c4d6e13c42a9ee708c182307
nitinscool <nitinscool@455121cc-eedf-11dd-924c-edcbac861201>
2009-03-10 15:37:48 +0000

    user.php modified

--- sys/managers/user.php
+++ sys/managers/user.php
@@ -1,3 +1,18 @@
 <?php
-    
+		function user_create($username,$password) {
+			global $_SETTINGS;
+			session_start();
+			if(isset($_SESSION['user']))
+				return FALSE;
+			$time=time();
+			$time_pass=md5($time);
+			$enc_pass=md5($password.$time_pass);
+			$_SESSION['user']=$username;
+			$_SESSION['password']=$enc_pass;
+			$query = "Insert into $_SETTINGS['database']['name']['table'](username,password) values($_SESSION ['user'],$_SESSION['password'])"; // just a dummy no such table exists
+			db_fetch_all(query);
+			session_unset();
+			session_destroy();
+			
+		}
 ?>
