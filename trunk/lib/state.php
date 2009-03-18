5194525693d1d4bfdaade381c3f01590c8fc6c00
srohit24 <srohit24@455121cc-eedf-11dd-924c-edcbac861201>
2009-03-18 10:17:18 +0000

    Updated the state.php again

--- trunk/lib/state.php
+++ trunk/lib/state.php
@@ -3,11 +3,11 @@
 function state_read($page, $appID,$varName)
 {
     $path = $page . ':' . $appID . ':' . $varName;
-    return $val = $_SESSION['$path'];
+    return $val = $_SESSION[$path];
 }
 function state_write($page, $appID,$varname, $value)
 {
     $path = $page . ':' . $appID . ':' . $varName;
-    $_SESSION['$path'] = $value;  
+    $_SESSION[$path] = $value;  
 }
 ?>
