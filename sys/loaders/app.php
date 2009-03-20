a3b87142d24f292509230a40876a68027468dbd6
prav.br <prav.br@455121cc-eedf-11dd-924c-edcbac861201>
2009-03-20 14:45:55 +0000

    Added caching feature for apps.

--- sys/loaders/app.php
+++ sys/loaders/app.php
@@ -1,20 +1,22 @@
+<?php if(!defined('KLONDIKE_VER')) die("Access denied!"); ?>
 <?php
 
     function app_load($app) {
         $funcName = $app['app'] . "_" . $app['view'] . "_view";
 
-        if(! function_exists($funcName)) { //Check if app's code was included. If not, include it.
+
+        if(! function_exists($funcName)) { // Check if app's code was included. If not, include it.
             require_once(WPATH . "bin" . DS . $app['app'] . DS . 'model.php');
             require_once(WPATH . "bin" . DS . $app['app'] . DS . 'controller.php');
             require_once(WPATH . "bin" . DS . $app['app'] . DS . 'view.php');
         }
         
-        if($app['controller'] != 'none') {
+        if($app['controller'] != 'none') { // Execute the Controller
             $funcName = $app['app'] . "_" . $app['controller'] . "_controller";
             call_user_func_array($funcName, array($app['parameters'], $app['subregions']));
         }
         
-        if($app['view'] != 'none') {
+        if($app['view'] != 'none') { // Execute the View
             $funcName = $app['app'] . "_" . $app['view'] . "_view";
             call_user_func_array($funcName, array($app['parameters'], $app['subregions']));
         }
